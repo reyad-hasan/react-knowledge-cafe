f4918c0764ca069c7dbb64dcd1ce80fc04396ccf
akmkamrulhasanreyad <akmkamrulhasanreyad@gmail.com>
2023-09-14 16:47:15 +0600

    create fake data and jetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl bg-red-400'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
